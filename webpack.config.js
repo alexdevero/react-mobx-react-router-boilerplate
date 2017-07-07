7368a7829e9834afb29b2017fce7a193cf974f5c
Alex Devero <deveroalex@gmail.com>
2017-07-07 13:46:19 +0200

    Add webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://0.0.0.0:1337',
+    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    'whatwg-fetch',
+    './src/index.js'
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: process.env.PORT || 1337,
+    host: '0.0.0.0',
+    publicPath: '/',
+    historyApiFallback: true,
+    disableHostCheck: true
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'app.[hash].js'
+  },
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              'es2015',
+              {
+                'modules': false
+              }
+            ],
+            'stage-0',
+            'react'
+          ],
+          plugins: [
+            'transform-async-to-generator',
+            'transform-decorators-legacy'
+          ]
+        }
+      },
+      {
+        test: /\.scss|css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'resolve-url-loader',
+          'sass-loader?sourceMap'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              optipng: {
+                optimizationLevel: 4
+              },
+              pngquant: {
+                quality: '75-90',
+                speed: 3
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'file-loader',
+        exclude: /images/,
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ hash: false, template: './index.hbs' }),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb/)
+  ]
+};
