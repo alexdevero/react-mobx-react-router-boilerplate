2e6875cf55bed5f3137f4251813fda906557139f
Alex Devero <deveroalex@gmail.com>
2018-07-29 09:49:45 +0200

    Upgrade dependencies, remove css sorter

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: [
     require('postcss-cssnext')(),
-    /*require('css-declaration-sorter')({
-      order: 'Concentric CSS'
-    }),*/
-    require('css-mqpacker')(),
+    require('css-mqpacker')()
   ]
-};
+}
